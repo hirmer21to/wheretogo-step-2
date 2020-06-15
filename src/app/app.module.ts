b391a105cf96dbaf3e741b6d4b65b2dd126d7828
Tobias Hirmer <tobias.hirmer@uni-bamberg.de>
2020-06-15 16:14:00 +0200

    copy from where to go repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WhereToGoComponent } from './where-to-go/where-to-go.component';
+import { LectureItemComponent } from './lecture-item/lecture-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WhereToGoComponent,
+    LectureItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
